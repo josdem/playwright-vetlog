7e7557bf541b9a886daeae6f88469d3c59afb3bc
josdem <joseluis.delacruz@gmail.com>
2023-09-24 17:09:56 -0400

    #2 Completing login test

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from "@playwright/test"
 import { Constants } from "../properties/test.properties"
 
-test("should list pets for adoption", async ({ page }) => {
+test("should login as a user", async ({ page }) => {
     await page.goto(Constants.URL)
     await page.getByRole('link', { name: /Login/ }).first().click()
     await expect(page).toHaveTitle(Constants.LOGIN_TITLE)
     await page.getByPlaceholder(/username/).fill(`${process.env.USERNAME}`)
     await page.getByPlaceholder(/password/).fill(`${process.env.PASSWORD}`)
+    await page.getByRole('button', { name: /Login/ }).click()
+    await expect(page.getByRole('link', { name: /Logout/ }).first()).toBeVisible()
 })
