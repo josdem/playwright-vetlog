b0c3a2ff9cadefd9c867419b74e355985afb226c
josdem <joseluis.delacruz@gmail.com>
2023-09-24 16:58:10 -0400

    #2 Adding vetlog url from constants

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test"
 import { Constants } from "../properties/test.properties"
 
 test("should list pets for adoption", async ({ page }) => {
-    await page.goto("https://vetlog.org/")
+    await page.goto(Constants.URL)
     await page.getByRole('link', { name: /Login/ }).first().click()
     await expect(page).toHaveTitle(Constants.LOGIN_TITLE)
     await page.getByPlaceholder(/username/).fill(`${process.env.USERNAME}`)
