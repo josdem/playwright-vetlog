58b2ab166421b3ea86ba613a46718bc2f893dac2
josdem <joseluis.delacruz@gmail.com>
2024-11-06 06:26:34 -0600

    #14 Adding login method

--- tests/utils/login.ts
+++ tests/utils/login.ts
@@ -1,10 +1,17 @@
 import { Page } from "@playwright/test"
-import { LoginPage } from "../pages/login.page"
+import { Constants } from "../properties/test.constants"
 
 export class Login {
   protected readonly page: Page
 
   constructor(page: Page) {
     this.page = page
+  }
+
+  async login(username: string, password: string): Promise<void> {
+    await this.page.goto(Constants.LOGIN_URL)
+    await this.page.getByPlaceholder(/username/).fill(username)
+    await this.page.getByPlaceholder(/password/).fill(password)
+    await this.page.getByRole("button", { name: "LOGIN" }).click()
   }
 }
