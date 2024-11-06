3347138f546cab4930a1b0fd17dbd86530089887
josdem <joseluis.delacruz@gmail.com>
2024-11-06 09:04:56 -0600

    #14 Adding pet create page

---
+++ tests/pages/pet.create.page.ts
@@ -0,0 +1,15 @@
+import { Page } from "playwright/test"
+import data from "../properties/data.json"
+
+export class PetCreatePage {
+  protected readonly page: Page
+
+  constructor(page: Page) {
+    this.page = page
+  }
+
+  async fillPetData(name: string): Promise<void> {
+    await this.page.getByPlaceholder(/name/).fill(data.petName)
+    await this.page.getByLabel("birth date").fill(data.petBirthDate)
+  }
+}
