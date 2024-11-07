9da171faf350576a2b36d5b60b8c6f76bafc241d
josdem <joseluis.delacruz@gmail.com>
2024-11-07 06:53:32 -0600

    #15 Adding delete pet test

--- tests/e2e/pet.registration.spec.ts
+++ tests/e2e/pet.registration.spec.ts
@@ -10,18 +10,29 @@ test.beforeAll(async () => {
     description: "https://github.com/josdem/vetlog-spring-boot/wiki/US1",
   })
   test.info().annotations.push({
+    type: "story",
+    description: "https://github.com/josdem/vetlog-spring-boot/wiki/US15",
+  })
+  test.info().annotations.push({
     type: "time",
     description: `${new Date()}`,
   })
 })
 
+let homePage: HomePage
+
 test("should registrer a pet", async ({ page }) => {
   let authenticator = new Authenticator(page)
-  let homePage = new HomePage(page)
+  homePage = new HomePage(page)
   let petCreatePage = new PetCreatePage(page)
   authenticator.login(`${process.env.VETLOG_USERNAME}`, `${process.env.VETLOG_PASSWORD}`)
   await homePage.clickOnRegisterPet()
   await expect(page).toHaveTitle(data.petCreateTitle)
   await petCreatePage.fillPetData()
   await expect(petCreatePage.getMessage()).toBeVisible()
+})
+
+test("should delete a pet", async ({ page }) => {
+  await homePage.clickOnRegisterPet()
+  await expect(page).toHaveTitle(data.petListTitle)
 })
